5b594601deaef84393a52900970ca3be76699221
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-07-01 20:42:55 -0700

    updated html routes to be linking correctly

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,5 +5,12 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
+    app.get("/home", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+      });
     
 }
