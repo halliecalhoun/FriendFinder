7c4f3655ed11ced5a03c82d86e2a872b772de1c5
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-07-01 20:27:04 -0700

    survey working properly with modal, but comparison match not working yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,14 +7,34 @@ module.exports = function(app) {
     });
 
     app.post('/api/friends', function(req, res) {
-        var userData = req.body;
-        var userScores = userData.scores
         var bestMatch = {
             name: "",
             photo: "",
-            difference: 500
+            matchDifference: 1000
             
         };
 
-       
+        var userData = req.body;
+        var userName = userData.name;
+        var userPhoto = userData.photo;
+        var userScores = userData.scores
+        var totalDifference = 0;
+
+
+        for (var i = 0; i < friends.length; i++) {
+            totalDifference = 0;
+            for (var j = 0; j < userScores.length; j++) {
+                totalDifference += Math.abs(parseInt(friends[i].scores[j] - (parseInt(userScores[j]))));
+
+                if (totalDifference <= bestMatch.difference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.matchDifference = totalDifference;
+                }
+            }
+        }
+        friends.push(userData);
+        res.json(bestMatch);
+        
+    });
 };
